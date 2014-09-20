fc77177af0dd46ab36e8abbca8cca374603f3fa0
nurahman <nurahman@ncsu.edu>
2014-09-20 16:39:56 -0400

    third attempt

--- direwolf/db/schema.rb
+++ direwolf/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140916023551) do
+ActiveRecord::Schema.define(version: 20140920040942) do
 
   create_table "applications", force: true do |t|
     t.string   "coverLetter"
@@ -36,6 +36,12 @@ ActiveRecord::Schema.define(version: 20140916023551) do
     t.string   "title"
     t.datetime "deadline"
     t.boolean  "isDeleted"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "jobseekers", force: true do |t|
+    t.string   "search"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
